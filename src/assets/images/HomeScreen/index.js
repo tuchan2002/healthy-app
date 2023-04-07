98cd5aae0b1a830ddfcbbdb9c97451d1460260c9
tuchan2002 <trananhtu171002@gmail.com>
2023-04-07 16:55:15 +0700

    feat: create UI for Home screen

---
+++ src/assets/images/HomeScreen/index.js
@@ -0,0 +1,6 @@
+export const homeScreenImages = {
+  home1: require("./home1.png"),
+  home2: require("./home2.png"),
+  home3: require("./home3.png"),
+  home4: require("./home4.png"),
+};
---
+++ src/screens/Heathy/Home/NavigationItem/Sleep.js
@@ -0,0 +1,30 @@
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { Image, TouchableOpacity, View } from "react-native";
+import { homeScreenImages } from "../../../../assets/images/HomeScreen";
+import CustomText from "../../../../components/CustomText";
+import styles from "../styles";
+
+const Sleep = () => {
+  const navigation = useNavigation();
+
+  return (
+    <TouchableOpacity
+      style={styles.itemContainer}
+      activeOpacity={0.5}
+      onPress={() => navigation.navigate("TargetsSetting")}
+    >
+      <CustomText style={[{ fontSize: 14 }]}>Ngủ</CustomText>
+      <Image
+        style={{
+          width: "100%",
+          height: 100,
+          resizeMode: "contain",
+        }}
+        source={homeScreenImages["home3"]}
+      />
+    </TouchableOpacity>
+  );
+};
+
+export default Sleep;
