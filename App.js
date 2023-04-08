404c96b10ec803af3e33582206fce57cb64b7340
tuchan2002 <trananhtu171002@gmail.com>
2023-04-08 10:20:19 +0700

    feat: create UI for WorkoutRecord Screen

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./src/screens/Heathy/Home";
 import TargetsSetting from "./src/screens/Heathy/TargetsSetting";
+import WorkoutRecord from "./src/screens/Heathy/WorkoutRecord";
 import Practice from "./src/screens/Practice";
 
 const Stack = createNativeStackNavigator();
@@ -15,6 +16,7 @@ export default function App() {
         }}
       >
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="WorkoutRecord" component={WorkoutRecord} />
         <Stack.Screen name="TargetsSetting" component={TargetsSetting} />
         <Stack.Screen name="Practice" component={Practice} />
       </Stack.Navigator>
---
+++ src/screens/Heathy/WorkoutRecord/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f0efef",
+    paddingHorizontal: 18,
+  },
+  overviewContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10,
+    marginBottom: 20,
+  },
+});
+
+export default styles;
