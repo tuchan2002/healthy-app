10038c449bf23fd08f7da7ea7c58602adb62b768
bach-long <nguyenbach19122002@gmail.com>
2023-04-12 00:51:13 +0700

    add exercises screen

---
+++ src/screens/Practice/Exercises/Option/index.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { StyleSheet, Pressable, Text } from "react-native";
+import { memo } from "react";
+
+const Option = ({ label }) => {
+  const [pressed, setPressed] = useState(false);
+
+  console.log({
+    ...styles.button,
+    backgroundColor: pressed
+      ? "rgba(255, 162, 57, 1)"
+      : "rgba(226, 221, 221, 1)",
+  });
+
+  return (
+    <Pressable
+      style={{
+        ...styles.button,
+        backgroundColor: pressed
+          ? "rgba(255, 162, 57, 1)"
+          : "rgba(226, 221, 221, 1)",
+      }}
+      onPress={(e) => {
+        setPressed((prev) => {
+          return !prev;
+        });
+      }}
+    >
+      <Text style={{ textAlign: "center", fontSize: 14, color: pressed
+          ? "rgba(255, 255, 255, 1)"
+          : "rgba(132, 132, 132, 1)"}}>{label}</Text>
+    </Pressable>
+  );
+};
+
+export default memo(Option);
+
+const styles = StyleSheet.create({
+  button: {
+    marginHorizontal: 2,
+    paddingVertical: 5,
+    width: 75,
+    color: "rgba(132, 132, 132, 1)",
+    backgroundColor: "rgba(226, 221, 221, 1)",
+    borderRadius: 20,
+  },
+});
