9f0d47997a4123a9beb8a8fb8c71a66e9a74e915
TrinhChung <chungtrinh2k2@gmail.com>
2023-04-13 02:21:50 +0700

    particle speed machine

--- src/screens/Heathy/Home/index.js
+++ src/screens/Heathy/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useState, useEffect, useRef} from "react";
 import { Image, ScrollView, TouchableOpacity, View } from "react-native";
 import CustomText from "../../../components/CustomText";
 import Layout from "../../../layouts/Layout";
@@ -8,9 +8,53 @@ import Target from "./NavigationItem/Target";
 import Sleep from "./NavigationItem/Sleep";
 import IBMIndex from "./NavigationItem/IBMIndex";
 import { useNavigation } from "@react-navigation/native";
-
+import { Accelerometer } from 'expo-sensors';
 export default function Home() {
   const navigation = useNavigation();
+  const [magnitudePrev, setMagnitudePrev] = useState(9)
+  const [change, setOnchange] = useState(false)
+  const [{ x, y, z }, setData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+  const steps = useRef(0)
+
+  const [subscription, setSubscription] = useState(null);
+
+  const _slow = () => Accelerometer.setUpdateInterval(1000);
+  const _fast = () => Accelerometer.setUpdateInterval(16);
+
+  const _subscribe = () => {
+    setSubscription(
+        Accelerometer.addListener((data)=> {
+          const x = data.x * 9.81;
+          const y = data.y * 9.81;
+          const z = data.z * 9.81;
+          const magnitude = Math.sqrt(x*x + y*y + z*z)
+          const magnitudeDelta  = magnitude - magnitudePrev;
+          setMagnitudePrev(magnitude)
+          if (magnitudeDelta > 3) {
+            steps.current += 1
+            setOnchange(!change)
+          }
+          setData(data)
+        })
+    );
+  };
+
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  useEffect(() => {
+    _subscribe();
+    return () => _unsubscribe();
+  }, []);
+
+
+
 
   return (
     <Layout>
@@ -26,7 +70,7 @@ export default function Home() {
               onPress={() => navigation.navigate("Footsteps")}
               activeOpacity={1}
             >
-              <CustomText style={[{ color: "white" }]}>--</CustomText>
+              <CustomText style={[{ color: "white" }]}>{steps.current}</CustomText>
               <CustomText style={[{ color: "white" }]}>bước</CustomText>
             </TouchableOpacity>
 
@@ -47,6 +91,15 @@ export default function Home() {
               0 kcal
             </CustomText>
           </View>
+        </View>
+        <View>
+          <CustomText>
+            Vui lòng cấp quyền đếm bước chân
+          </CustomText>
+          <CustomText style={styles.text}>Accelerometer: (in gs where 1g = 9.81 m/s^2)</CustomText>
+          <CustomText style={styles.text}>x: {x}</CustomText>
+          <CustomText style={styles.text}>y: {y}</CustomText>
+          <CustomText style={styles.text}>z: {z}</CustomText>
         </View>
 
         <ScrollView
