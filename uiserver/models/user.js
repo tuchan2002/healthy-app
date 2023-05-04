52f76362205f4d393839442abe0281222ae88229
bach-long <nguyenbach19122002@gmail.com>
2023-05-04 22:15:33 +0700

    sync api

--- uiserver/models/user.js
+++ uiserver/models/user.js
@@ -7,11 +7,12 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ BMI, UserTarget, UserTargetState, FootStep }) {
+    static associate({ BMI, UserTarget, UserTargetState, FootStep, SyncedStep }) {
       this.hasOne(BMI, { foreignKey: "user_id" });
       this.hasMany(UserTarget, { foreignKey: "user_id" });
       this.hasMany(UserTargetState, { foreignKey: "user_id" });
       this.hasMany(FootStep, { foreignKey: "user_id" });
+      this.hasMany(SyncedStep, { foreignKey: "user_id" });
     }
   }
   User.init(
--- uiserver/routes/users.js
+++ uiserver/routes/users.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var { User } = require("../models");
+const SyncedStep = require('../models/syncedstep');
 
 /* GET user by user_id. */
 router.get("/", function (req, res, next) {
@@ -29,5 +30,13 @@ router.post("/login", async (req, res) => {
     res.json({ success: 0, message: error.message });
   }
 });
+
+route.post('/sync', async (req, res) => {
+  req.steps.map(element => element["user_id"] = req.id);
+  const result = await FootStep.bulkCreate(req.steps, {updateOnDuplicate: true});
+  if(result) {
+    res.json({success: 1, message: 'data synced'});
+  }
+})
 
 module.exports = router;
